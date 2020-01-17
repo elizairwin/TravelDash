a8c5498e0cecee1801955e548ac98f1ead6334e5
Eliza Irwin <elizairwin@gmail.com>
2020-01-17 18:33:21 -0500

    minor adds to css

--- travel-dash/src/components/Main/main.jsx
+++ travel-dash/src/components/Main/main.jsx
@@ -15,27 +15,28 @@ export default class Main extends Component {
                         <div className="inner">
                             {/* Header */}
                             <header id="header">
-                                {/* <a href="index.html" class="logo"><strong>Editorial</strong> by HTML5 UP</a> */}
-                                <ul className="header">
+                                <h1>TravelDash</h1>
+                                <h2>Stay up-to-date on the go</h2>
+                                <ul className="links">
                                     <li><a href=""><span className="label">Flight</span></a></li>
                                     <li><a href=""><span className="label">Hotel</span></a></li>
                                     <li><a href=""><span className="label">Rental Car</span></a></li>
                                     <li><a href=""><span className="label">Credit Card</span></a></li>
                                 </ul>
                             </header>
-                            {/* Banner */}
+                            {/* Dashboard Options */}
                             <section id="dashboards">
                                 <div className="content">
-                                    <header>
-                                        <h1>Hi there, <br /> Welcome to TravelDash</h1>
-                                    </header>
-                                    <p></p>
-                                    <ul className="contact">
-                                        <li className="icon solid fa-envelope"><a href="mailto:elizairwin@gmail.com">elizairwin@gmail.com</a></li>
-                                        <li className="icon solid fa-phone">(614) 257-7877</li>
-                                    </ul>
-                                   </div>
-                            </section></div></div></div></div>
+                                    Flight Card
+                                    Hotel Card
+                                    Rental Car Card
+                                    Credit Card Card
+                                </div>
+                            </section>
+                        </div>
+                    </div>s
+                </div>
+            </div>
         );
     }
 };
